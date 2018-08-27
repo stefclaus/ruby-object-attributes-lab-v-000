975e462dd7da7bdde975ea5344c50f56a6801205
Stef Claus <stef.claus@gmail.com>
2018-08-27 12:37:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,11 @@
 class Dog 
-  def initialize(name)
-    @name = name
+   def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
  
   def name
-    @name
+    @this_dogs_name
   end
- 
-  def name=(new_name)
-    @name = new_name
-  end
-  
-  def initialize(breed)
-    @breed = breed
-  end
- 
-  def breed
-    @breed
-  end
- 
-  def breed=(new_breed)
-    @breed = new_breed
-  end 
   
 end 
 
